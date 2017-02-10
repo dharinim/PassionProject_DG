3ca804656c80c1f4176b89e64b67b88ddcf5e1d1
georgecode <georgeczurlanis@gmail.com>
2017-02-10 12:33:03 -0800

    Add spin and stuff

--- public/js/application.js
+++ public/js/application.js
@@ -6,6 +6,11 @@ $(document).ready(function() {
 function embbedImage(e){
 
   e.preventDefault();
+  // $('.spiral').delay(6000).show();
+
+$('.allQuestions').css('background-color','black  ')
+
+
   console.log('embedding image');
   link = $(this);
   var url = link.attr('action');
@@ -25,7 +30,28 @@ function embbedImage(e){
     // res = r.find('div')
     // console.log(res)
     // res.on('click', likePost);
-    link.find('.gif').html(response);
+    // link.find('.gif').html(response).delay(4000);
+    link.find('.spiral').fadeIn().delay(4000).queue(function(){
+  link.find('.spiral').show();
+  $('.allQuestions').css('background-color','rgba(102, 153, 153, .1)')
+  link.find('.spiral').hide();
+});
+
+    link.find('.gif').fadeIn().delay(4000).queue(function(){
+      link.find('.gif').html(response);
+
+    })
+
+
+
+
+
+
+
+//   $('.spiral').fadeIn().delay(4000).queue(function(){
+//   $('.spiral').show();
+//   $('.spiral').hide();
+// });
 
     // $('#new-post-form').get(0).reset()
   })
